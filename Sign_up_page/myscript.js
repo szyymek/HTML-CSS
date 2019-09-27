99863504e1ae96ed88247e57885e55f9a6cd59a4
szyymek <45272766+szyymek@users.noreply.github.com>
2019-09-27 12:53:07 +0200

    Create myscript.js

---
+++ Sign_up_page/myscript.js
@@ -0,0 +1,10 @@
+alert("Welcome to sign page")
+var payment = prompt("How much do you want to donate our project?")
+if (payment>100) {
+alert("You've donated: "+payment)
+alert("You are a cool person")
+}
+else{
+alert("You've donated: "+payment)
+alert("It is little donation")
+}
